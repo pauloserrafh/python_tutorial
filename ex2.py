871dc26374cae2f3cfc0000a7e3f9649875d3b93
Paulo Serra Filho <pauloserrafh@gmail.com>
2017-12-13 03:05:51 -0300

    Parte2-cont

--- ex2.py
+++ ex2.py
@@ -37,12 +37,37 @@ def lista2dic(lista):
         tmp = tmp[nome]
     return dic
 
+def div9():
+    for x in range(30):
+        if not x % 9:
+            continue
+        print(x)
+
+def find_key(dic):
+    chaves = dic.keys()
+    for chave in chaves:
+        if chave == "teste":
+            print("O valor da chave teste é %(teste)s" %dic)
+            break
+    else:
+        print("Chave não encontrada")
 
 if __name__ == '__main__':
     a = [2,4,3,5,7]
     b = [4,3]
     c = [3,7]
+    d = {'Debian': 'apt-get',
+        'Ubuntu': 'apt-get',
+        'Fedora': 'dnf',
+        'CentOS': 'yum',
+        'OpenSUSE': 'zypper',
+        'Arch': 'pacman',
+        'Gentoo': 'emerge',
+        'teste': 'encontrado'
+    }
     print(sublista(a, b))
     print(sublista(a, c))
     print(segundo_maior([4, 6, 5, 1, 9]))
     print(lista2dic([4, 6, 5, 1, 9]))
+    div9()
+    find_key(d)
