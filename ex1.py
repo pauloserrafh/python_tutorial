741873376f455f09ff9631186a0bd9f4053c0ed1
Paulo Serra Filho <pauloserrafh@gmail.com>
2017-12-13 01:27:16 -0300

    Parte 1

--- ex1.py
+++ ex1.py
@@ -5,13 +5,35 @@ def is_palindrome(x):
         return False
     return True
 
-
 def concat_ends(st):
     if len(st) < 2:
         return ""
     return st[0:2] + st[-2:]
 
+def print_tupla(tp):
+    return (tp[2],tp[-5])
+
+def lista_vazia(lista):
+    if not lista:
+        print("Lista vazia")
+    else:
+        print("Lista contem ao menos um elemento")
+
+def max_dois(x, y):
+    if x > y:
+        return x
+    return y
+
+def max_tres(x, y, z):
+    return max_dois(x, max_dois(y, z))
+
+def lista2string(lista):
+    return "".join(lista)
 
 if __name__ == '__main__':
     print(is_palindrome("arara"))
     print(concat_ends("asdzxc"))
+    print(print_tupla((1,2,3,4,5,6)))
+    lista_vazia([])
+    print(max_tres(3, 6, -5))
+    print(lista2string(['p','y','t','h','o','n']))
--- hello.py
+++ hello.py
@@ -1,6 +1,20 @@
 print("Hello, World!")
 
+# # Funções
+# def minha_soma(a, b):
+#     return a+b
+
+# def calculadora(a, b):
+#     return a+b,a-b
+
+# def parametro_nomeado(a = "Paulo"):
+#     print(a)
+
 # if __name__ == '__main__':
 #     print("Hello, World!")
+#     print(minha_soma(1,2))
+#     print(calculadora(1,2))
+#     parametro_nomeado()
+#     parametro_nomeado("Serra")
 # else:
 #     print("Estou sendo importado")
