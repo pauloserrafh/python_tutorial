0f316a284c6a739292ca75e971472a5ee0916799
Paulo Serra Filho <pauloserrafh@gmail.com>
2018-05-01 22:11:35 -0300

    Finalizando exemplos e exercicios Pandas

--- ex5.py
+++ ex5.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
 
 def series_to_list():
     ds = pd.Series([2, 4, 6, 8, 10])
@@ -20,6 +24,46 @@ def compara():
     print("Maior que:")
     print(ds1 > ds2)
 
+def dataFrame_operacoes():
+    df = pd.DataFrame(exam_data)
+    print('Data Frame')
+    print(df)
+    print('Media Score:')
+    print(df['score'].mean())
+    print('Media Tentativas:')
+    print(df['attempts'].mean())
+    print('Tentaram mais de 1 vez:')
+    print(df[(df['attempts'] > 1) & (df['qualify'] == 'no')])
+
+def newData():
+    df = pd.DataFrame(exam_data)
+    value = {"name" : "Suresh", "score": 15.5, "attempts": 1, "qualify": "yes"}
+    df2 = df.append(value, ignore_index=True)
+    print(df2)
+    df2.to_csv('newData.csv', index=False)
+
+def final():
+    df = pd.read_csv('final.csv')
+    ratio = []
+    for index, row in df.iterrows():
+        rt = row['score']/row['attempts']
+        ratio.append(rt)
+    df['ratio'] = ratio
+
+    maximo = df["ratio"].max()
+    minimo = df["ratio"].min()
+    print("Maior ratio:")
+    print(df[df["ratio"] == maximo])
+    print("Menor ratio:")
+    print(df[df["ratio"] == minimo])
+
+    df = df.sort_values(by=['ratio'], ascending=[False])
+
+    df.to_csv('final_update.csv',index=False)
+
 if __name__ == '__main__':
     # series_to_list()
-    compara()
+    # compara()
+    # dataFrame_operacoes()
+    # newData()
+    final()
--- parte5.py
+++ parte5.py
@@ -28,21 +28,50 @@ import numpy as np
 # print(sd[1:]+sd[:-1])
 
 
-d = {'one' : pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
-    'two' : pd.Series([1., 2., 3., 4.], index=['a', 'b', 'c', 'd'])}
+# d = {'one' : pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
+#     'two' : pd.Series([1., 2., 3., 4.], index=['a', 'b', 'c', 'd'])}
 
-df = pd.DataFrame(d)
-print(df)
+# df = pd.DataFrame(d)
+# print(df)
 
-print(df.index)
-print(df.columns)
-print(df['one']['a'])
+# print(df.index)
+# print(df.columns)
+# print(df['one']['a'])
 
 # DataFrame a partir de uma lista de dicionários
-data2 = [{'a': 1, 'b': 2}, {'a': 5, 'b': 10, 'c': 20}]
+# data2 = [{'a': 1, 'b': 2}, {'a': 5, 'b': 10, 'c': 20}]
+
 # Sem nomear indice explicitamente
-pd.DataFrame(data2)
+# pd.DataFrame(data2)
+
 # Nomeando indices explicitamente
-print(pd.DataFrame(data2, index=['first', 'second']))
+# print(pd.DataFrame(data2, index=['first', 'second']))
+
 # Restringindo colunas
-print(pd.DataFrame(data2, columns=['a', 'b']))
+# print(pd.DataFrame(data2, columns=['a', 'b']))
+
+# Usando funcoes nativas e comparacoes
+exam_data  = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+        'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+        'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(exam_data , index=labels)
+# print(df)
+# print("\nMaior score no dataFrame:")
+# max_score = df['score'].max()
+# print(df[df['score'] == max_score])
+
+
+# Leitura e escrita de CSV
+# df.to_csv('exemplo.csv')
+# df.to_csv('exemplo_noindex.csv', index=False)
+# df.to_csv('exemplo_noheader.csv', header=False)
+
+# df2 = pd.read_csv('exemplo.csv', index_col=0)
+# print(df2)
+# df2 = pd.read_csv('exemplo_noindex.csv')
+# print(df2)
+df2 = pd.read_csv('exemplo_noheader.csv', index_col=0, header=None)
+print(df2)
